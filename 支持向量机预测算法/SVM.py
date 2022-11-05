526dfe6ba49fcd8c2399f902d5415c02afafdedf
SEMHAQ <2990280345@qq.com>
2022-11-05 19:50:22 +0800

    支持向量机补充

--- 支持向量机预测算法/SVM.py
+++ 支持向量机预测算法/SVM.py
@@ -11,15 +11,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import preprocessing
-from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 from sklearn.svm import SVR
-
-
-# 辅助函数
-def cross_val(model, X, Y):
-    pred = cross_val_score(model, X, Y, cv=10)
-    return pred.mean()
 
 
 def print_evaluate(true, predicted):
@@ -27,10 +20,10 @@ def print_evaluate(true, predicted):
     mse = metrics.mean_squared_error(true, predicted)
     rmse = np.sqrt(metrics.mean_squared_error(true, predicted))
     r2_square = metrics.r2_score(true, predicted)
-    print('MAE:', mae)
-    print('MSE:', mse)
-    print('RMSE:', rmse)
-    print('R2 Square', r2_square)
+    print('MAE:', mae)  # 均方误差
+    print('MSE:', mse)  # 均方根误差
+    print('RMSE:', rmse)  # 平均绝对误差
+    print('R2 Square', r2_square)  # R方分数
     print('__________________________________')
 
 
@@ -52,21 +45,28 @@ X_test = features[split_num:]
 Y_test = target[split_num:]
 # 支持向量机建模
 svm_reg = SVR(kernel='rbf', C=30, epsilon=0.01)
-print(cross_val(svm_reg, X_train, Y_train))
+
 svm_reg.fit(X_train, Y_train)
-test_pred = svm_reg.predict(X_test)
-train_pred = svm_reg.predict(X_train)
+
+test_predict = svm_reg.predict(X_test)
+train_predict = svm_reg.predict(X_train)
 print('Test set evaluation:\n_____________________________________')
-print_evaluate(Y_test, test_pred)
+print_evaluate(Y_test, test_predict)
 print('Train set evaluation:\n_____________________________________')
-print_evaluate(Y_train, train_pred)
+print_evaluate(Y_train, train_predict)
+
+# 将结果在指定路径保存为csv
+# data -> 原始数据 ; result -> 预测数据 ; diff -> 原始数据与预测数据之差
+df = pd.DataFrame({'data': Y_test, 'result': test_predict, 'diff': Y_test - test_predict})
+df.to_csv(r'E:\python-machine-learning\支持向量机预测算法\result.csv', index=False)
+
 # 可视化部分
-sns.set(font_scale=1.2)
-plt.rcParams['font.sans-serif'] = 'SimHei'
-plt.rcParams['axes.unicode_minus'] = False
-plt.rc('font', size=14)
-plt.plot(list(range(0, len(X_test))), Y_test, marker='o')
-plt.plot(list(range(0, len(X_test))), test_pred, marker='*')
-plt.legend(['真实值', '预测值'])
-plt.title('Boston房价支持向量机预测值与真实值的对比')
-plt.show()
+# sns.set(font_scale=1.2)
+# plt.rcParams['font.sans-serif'] = 'SimHei'
+# plt.rcParams['axes.unicode_minus'] = False
+# plt.rc('font', size=14)
+# plt.plot(list(range(0, len(X_test))), Y_test, marker='o')
+# plt.plot(list(range(0, len(X_test))), test_predict, marker='*')
+# plt.legend(['真实值', '预测值'])
+# plt.title('Boston房价支持向量机预测值与真实值的对比')
+# plt.show()
