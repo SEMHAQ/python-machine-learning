465df9ff4fcddcb3999cd54ece85a107c1883b66
SEMHAQ <2990280345@qq.com>
2022-11-05 20:10:30 +0800

    聚类补充

--- K-means聚类算法/kmeans.py
+++ K-means聚类算法/kmeans.py
@@ -8,6 +8,7 @@
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn import datasets
+import pandas as pd
 
 # 直接从sklearn中获取数据集
 from sklearn.datasets import load_iris
@@ -45,6 +46,11 @@ x2 = X[label_pred == 2]
 print(label_pred)
 print(iris_target)
 print("数据总数：", len(X), "  错误个数：", (label_pred != iris_target).sum())
+
+df = pd.DataFrame(
+    {'Column_1': X[:, 0], 'Column_2': X[:, 1], 'Column_3': X[:, 2], 'Column_4': X[:, 3], 'data': iris_target,
+     'result': label_pred, 'diff': label_pred == iris_target})
+df.to_csv(r'E:\python-machine-learning\K-means聚类算法\result.csv', index=False)
 
 # plt.scatter(x0[:, 0], x0[:, 1], c="red", marker='o', label='label0')
 # plt.scatter(x1[:, 0], x1[:, 1], c="green", marker='*', label='label1')
