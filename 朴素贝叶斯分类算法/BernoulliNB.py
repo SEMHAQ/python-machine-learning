a39ea5c39010c45558d376449e1f982728547412
SEMHAQ <2990280345@qq.com>
2022-11-05 13:53:32 +0800

    朴素贝叶斯基本架构完成

--- 朴素贝叶斯分类算法/BernoulliNB.py
+++ 朴素贝叶斯分类算法/BernoulliNB.py
@@ -9,45 +9,27 @@
 from sklearn.datasets import load_iris
 
 # 数据选取
+from sklearn.model_selection import cross_val_score
+
 iris_data = load_iris()['data']
 iris_target = load_iris()['target']
 
-# 用高斯模型进行预测并评估
-from sklearn.naive_bayes import GaussianNB
-mol = GaussianNB()
-result = mol.fit(iris_data,iris_target)
-pred1 = mol.predict(iris_data)
-# 对模型进行评估
-from sklearn.model_selection import cross_val_score
-scores = cross_val_score(mol,iris_data,iris_target,cv=10)
-# 对预测结果的正确个数进行计算
-print("高斯模型：")
-print("数据总数：",len(iris_data),"  错误个数：",(iris_target != pred1).sum())
-print("Accuracy:%.3f"%scores.mean())
+# 数据预览
+# print(iris_data)
+# print(iris_target)
 
-# 用贝努里模型进行预测和评估
+# 用伯努利模型进行预测和评估
 from sklearn.naive_bayes import BernoulliNB
-bnb = BernoulliNB()
-result2 = bnb.fit(iris_data,iris_target)
-pred2 = bnb.predict(iris_data)
-# 计算错误个数
-print("贝努里模型：")
-print("数据总数：",len(iris_data),"  错误个数：",(iris_target != pred2).sum())
-#模型评分
-scores2 = cross_val_score(bnb,iris_data,iris_target)
-print("Accuracy:%.3f"%scores2.mean())
 
-# 用多项式建立模型进行预测和评估
-from sklearn.naive_bayes import MultinomialNB
-mnb = MultinomialNB()
-result3 = mnb.fit(iris_data,iris_target)
-# 预测
-pred3 = result3.predict(iris_data)
+model = BernoulliNB()
+result = model.fit(iris_data, iris_target)
+predict = model.predict(iris_data)
+
 # 计算错误个数
-print("多项式模型：")
-print("数据总数：",iris_data.shape[0],"  错误个数：",(iris_target != pred3).sum())
+print("伯努利模型：")
+print("数据总数：", len(iris_data), "  错误个数：", (iris_target != predict).sum())
 # 模型评分
-scores3 = cross_val_score(mnb,iris_data,iris_target)
-print("Accuracy:%.3f"%scores3.mean())
+scores = cross_val_score(model, iris_data, iris_target)
+print("Accuracy:%.3f" % scores.mean())
 
 
--- 朴素贝叶斯分类算法/MultinomialNB.py
+++ 朴素贝叶斯分类算法/MultinomialNB.py
@@ -5,27 +5,32 @@
 # File:MultinomialNB.py
 # Software:PyCharm
 import pandas as pd
-import numpy as np
+# 导入鸢尾花数据集
+from sklearn.datasets import load_iris
+from sklearn.model_selection import cross_val_score
 
-X = np.array([[1.14, 1.78], [1.18, 1.96], [1.20, 1.86], [1.26, 2.00], [1.28, 2.00],
-              [1.30, 1.96], [1.24, 1.72], [1.36, 1.74], [1.38, 1.64], [1.38, 1.82],
-              [1.38, 1.90], [1.40, 1.70], [1.48, 1.82], [1.54, 1.82], [1.56, 2.08]])
-Y = np.hstack((np.ones(6), np.ones(9) * 2))  # 数组合并
 
+# 数据选取
+iris_data = load_iris()['data']
+iris_target = load_iris()['target']
+
+# 数据预览
+# print(iris_data)
+# print(iris_target)
+
+# 用多项式建立模型进行预测和评估
 from sklearn.naive_bayes import MultinomialNB
 
 model = MultinomialNB()
-model.fit(X, Y)
-
-print("预测结果")
-print(model.predict([[1.24, 1.80]]))
-print("样本为1类的概率")
-print(model.predict_proba([[1.24, 1.80]]))
-print("样本为2类的概率")
-print(model.predict_log_proba([[1.24, 1.80]]))
-
-print("预测结果")
-print(model.predict([[1.29, 1.81], [1.43, 2.03]]))
+result = model.fit(iris_data, iris_target)
+# 预测
+predict = result.predict(iris_data)
+# 计算错误个数
+print("多项式模型：")
+print("数据总数：", iris_data.shape[0], "  错误个数：", (iris_target != predict).sum())
+# 模型评分
+scores = cross_val_score(model, iris_data, iris_target)
+print("Accuracy:%.3f" % scores.mean())
 
 
 
